ff6c70c1a4c7961d17d2d21659883a4e17353bf1
alexey.dzyk <alexey.dzyk@yandex.ru>
2025-08-25 04:24:30 +0300

    Полная перезапись файлов

--- db/init.sql
+++ db/init.sql
@@ -47,8 +47,8 @@ ON CONFLICT DO NOTHING;
 
 -- Пароли захешированы bcrypt (cost=12)
 INSERT INTO users (email, password_hash, name, role) VALUES
-('admin@example.com', '$2a$12$Qm1yH0y3Jx4k1t2rJ7UawOVT8a0XKfXf0YyES5Z3wXK6jYv1wM2Dm', 'Admin', 'admin'),
-('business@example.com', '$2a$12$h5Qz3VQkqQd9a6d2M4n4gO2w2a0YqG3l1qgYqXo0mNmwqI2d1Zr3m', 'Business User', 'business')
+('admin@example.com', '$2b$12$jo/qhANdAyqikVqJkAHmB.vn1/gVgjmjXHHjAd17b5vWcuMnrcjDa', 'Admin', 'admin'),
+('business@example.com', '$2b$12$jo/qhANdAyqikVqJkAHmB.vn1/gVgjmjXHHjAd17b5vWcuMnrcjDa', 'Business User', 'business')
 ON CONFLICT DO NOTHING;
 -- (admin123) -> $2a$12$Qm1yH0y3Jx4k1t2rJ7UawOVT8a0XKfXf0YyES5Z3wXK6jYv1wM2Dm
 -- (business123) -> $2a$12$h5Qz3VQkqQd9a6d2M4n4gO2w2a0YqG3l1qgYqXo0mNmwqI2d1Zr3m
